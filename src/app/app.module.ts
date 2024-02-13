5efe1be73c08090e4eec1bd3fca5b4ef79124b63
Saidinesh <saidineshgrandhe777@gmail.com>
2024-02-13 11:00:07 +0530

    Added update page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,15 @@ import { AddComponent } from './add/add.component';
 import { FormsModule} from "@angular/forms";
 import { ClientComponent } from './client/client.component';
 import { DeleteComponent } from './delete/delete.component';
+import { UpdateComponent } from './update/update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddComponent,
     ClientComponent,
-    DeleteComponent
+    DeleteComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
